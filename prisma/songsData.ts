23054b21695cc423ece90938c8ba2bfcbc07fb85
amrn1991 <amrfst91@gmail.com>
2025-03-15 12:56:08 +0330

    seeding data into db

---
+++ prisma/songsData.ts
@@ -0,0 +1,62 @@
+interface Data {
+  name: string
+  songs: any[]
+}
+
+export const artistsData: Data[] = [
+  {
+    name: 'عرفان طهماسبی',
+    songs: [
+      {
+        name: 'هزار و یک شب',
+        duration: 176,
+        url: 'https://dls.musics-fa.com/song/alibz/dlswm/Erfan%20Tahmasbi%20-%20Hezaro%20Yek%20Shab%20(320).mp3',
+      },
+    ],
+  },
+  {
+    name: 'محسن چاووشی',
+    songs: [
+      {
+        name: 'کولی',
+        duration: 83,
+        url: 'https://dls.musics-fa.com/song/alibz/dlswm/Mohsen%20Chavoshi%20-%20Koli%20(320).mp3',
+      },
+    ],
+  },
+  {
+    name: 'بهنام بانی',
+    songs: [
+      {
+        name: 'زخم کاری',
+        duration: 207,
+        url: 'https://dls.musics-fa.com/tagdl/ati/Behnam%20Bani%20-%20Zakhm%20Kari%20(128).mp3',
+      },
+    ],
+  },
+  {
+    name: 'علیرضا قربانی',
+    songs: [
+      {
+        name: 'شیدا',
+        duration: 230,
+        url: 'https://dls.musics-fa.com/song/alibz/dlswm/Alireza%20Ghorbani%20-%20Sheyda%20(320).mp3',
+      },
+    ],
+  },
+  {
+    name: 'شروین حاجی پور',
+    songs: [
+      {
+        name: 'تا کی؟',
+        duration: 185,
+        url: 'https://dls.musics-fa.com/song/alibz/dlswm/Shervin%20Hajiaghapour%20-%20Ta%20Key%20(320).mp3',
+      },
+      {
+        name: 'شبونه',
+        duration: 200,
+        url: 'https://dls.musics-fa.com/song/alibz/1403/Shervin%20Hajiaghapour%20-%20Shaboone%20(320).mp3',
+      },
+    ],
+  },
+]
