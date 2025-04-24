b2c6c26af64be3eb6980f8d53f7018c8bb733474
amrn1991 <amrfst91@gmail.com>
2025-04-24 16:58:18 +0330

    another change

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,6 +1,6 @@
 import { validateRoute } from '@/lib/auth'
 
-export async function GET(req: Request, res: Response) {
+export async function GET(req: Request) {
   const result = await validateRoute()
 
   return Response.json(result)
